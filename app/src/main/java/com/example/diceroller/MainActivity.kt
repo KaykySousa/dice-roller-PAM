df0e73b7484d88eae12575d17f276dd6243d1d98
KaykySousa <kaykysousa321@gmail.com>
2022-03-13 19:10:25 -0300

    :tada: Second Commit :tada:

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +17,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val randomInt = (1..6).random()
+
+        val image = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(image)
+
+        Toast.makeText(this, randomInt.toString(), Toast.LENGTH_SHORT).show()
     }
 }
